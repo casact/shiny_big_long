fc7e6446347f0a6c909614ce77ee673b8b44f888
Josh <josh@pop-os.localdomain>
2019-04-25 22:14:08 -0500

    Tweedie too slow simulation, moved to freqxsev

--- db_Setup.R
+++ db_Setup.R
@@ -10,12 +10,6 @@ library(data.table)
 if(file.exists('./CAS_LTPAWP.sqlite')) file.rename('./CAS_LTPAWP.sqlite','./CAS_LTPAWP.sqlite.bak')
 con<-dbConnect(SQLite(),"CAS_LTPAWP.sqlite") #This creates the data base
 
-#' TABLE THAT WILL HOLD ALL THE LOSS AND PERSISTENCY CLASS NAMES
-dbWriteTable(conn=con
-             ,name = 'InsuredClass'
-             ,value = data.frame(Class='loss_x_persist',Loss_Class='loss',Persistency_Class='persist')
-            )
-
 #' TABLE TO HOLD THE PREMIUM MOVES OVER TIME.
 #' This would be what we read from mid-round to show the market moves and what we write to with a submit button.
 #' Users will need to provide:
@@ -41,23 +35,25 @@ dbWriteTable(conn=con
 
 #' GENERATE INSURED CLASSES, INSURED LIST, AND STARTER EXPERIENCE
 #' Basically just a 1M or so records strewn between loss and persistency class names.
-market_size<-1000000
+market_size<-50000
 
-#' I'm going to set up a set of tweedie models for which the mean, power, and phi will be set.
-#' We can achieve several distribuitons from Tweedie assuming we pick the parameters.
-#' If you don't like Tweedie, then be my guest and implement a different model.
+#' I'm going to set up a set of freq x severity models for which the paramters will be set
+#' Originally wanted to go with Tweedie, but terribly slow simulations with rtweedie.
+#' I'm going for a gamma binomial mix at this point (keep it to one claim per insured)
 #' All the fixed values are 100% arbitrary as of this point.
 loss_classes<-c('A','B','C','D','E')
-loss_means<-seq(1000,by=100,length.out = length(loss_classes))+rnorm(n=length(loss_classes),mean=25,sd=5)
-loss_power<-runif(length(loss_classes))*0.66+1
-loss_phi<-rep(1,length(loss_classes))
+loss_means_sev<-seq(1000,by=100,length.out = length(loss_classes))+rnorm(n=length(loss_classes),mean=25,sd=5)
+loss_means_freq<-seq(0,by=.01,length.out = length(loss_classes))+runif(n=length(loss_classes))/50
 
-loss_models<-data.table(cbind(loss_classes,loss_means,loss_power,loss_phi))
+#We could add a trend column here so that when we simulation we do the mean*trend^round
+
+loss_models<-data.table(Loss_Class=loss_classes
+                        ,loss_means_sev=loss_means_sev
+                        ,loss_means_freq=loss_means_freq
+                        )
 
 #' going to join everything but the class_names (loss_classes)
-add_loss_names<-setdiff(names(loss_models),'loss_classes') 
-
-rm(loss_classes,loss_means,loss_power,loss_phi)
+add_loss_names<-setdiff(names(loss_models),'Loss_Class') 
 
 #' I'm going to set up a set of binomials models for which the mean will depend on rate change and 
 #' distance to market minimum price. There is an additional parameter for 'time with company' controlling
@@ -66,12 +62,13 @@ persistency_classes<-c('loayalist','moderate','mover')
 rate_change_mult<-c(0.25,0.5,1.0)
 market_price_mult<-c(-0.25,0.05,0.5)
 general_cancel_rate<-rep(.02,length(persistency_classes))
-persist_models<-data.table(cbind(persistency_classes,general_cancel_rate,rate_change_mult,market_price_mult))
+persist_models<-data.table(Persistency_Class=persistency_classes
+                           ,general_cancel_rate
+                           ,rate_change_mult
+                           ,market_price_mult)
 
 #' going to join everything but the class_names (loss_classes)
-add_persist_names<-setdiff(names(persist_models),'persistency_classes') 
-
-rm(persistency_classes,general_cancel_rate,rate_change_mult,market_price_mult)
+add_persist_names<-setdiff(names(persist_models),'Persistency_Class') 
 
 
 #' Now we can set up our initial table of insureds with associated class parameters.
@@ -83,15 +80,22 @@ insured_data<-data.table(Insured=paste0(LETTERS,sample(1000000:9999999,size=mark
 insured_data[,(add_loss_names):=loss_models[insured_data[,.(Loss_Class)]
                                         ,.SD
                                         ,.SDcols=add_loss_names
-                                        ,on=.(loss_classes=Loss_Class)]
+                                        ,on=.(Loss_Class)]
              ]
 
 insured_data[,(add_persist_names):=persist_models[insured_data[,.(Persistency_Class)]
                                         ,.SD
                                         ,.SDcols=add_persist_names
-                                        ,on=.(persistency_classes=Persistency_Class)]
+                                        ,on=.(Persistency_Class)]
              ]
 
+insured_data[,`:=`(As_of_Round=0
+                   ,Premium=50
+                   ,Loss=rbinom(n=1:nrow(insured_data),1,loss_means_freq)*rgamma(n=1:nrow(insured_data),shape = loss_means_sev,rate = 1)
+                   ,Lapse=0
+                   ,Insurer="STATE"
+                  )
+             ]
 
 #'AT THIS POINT WE WOULD HAVE A DATABASE OF INSUREDS
 #'
