2337b1ba5d64c0345193eae230d531ae066a26a9
Josh <josh@pop-os.localdomain>
2019-04-25 23:44:18 -0500

    add New_premium join

--- round_update.R
+++ round_update.R
@@ -10,15 +10,20 @@ con<-dbConnect(SQLite(),"CAS_LTPAWP.sqlite")
 
 premium_data<-dbGetQuery(con,
                          "SELECT * FROM Market_Premiums")
+setDT(premium_data)
 
 #insured_data<-dbGetQuery(con,
 #                paste0("SELECT * FROM Insured_File WHERE As_of_Round>",round-2))) #grab last two rounds to compute prem diffs
 
 #' Pair in new premium information to losses
-insured_data[,New_Premium:=premium_data]
+insured_data[,New_Premium:=premium_data[insured_data[,.(Insurer,As_of_Round,Loss_Class,Persistency_Class)]
+                                       ,Premium
+                                       ,on=.(Insurer,Loss_Class,Persistency_Class,As_of_Round)
+                                       ,roll=Inf]]
 
 #' Calcualte min market rate for LossxPersistency class
 #' Calcualte premium change (shift by Insured and round)
 #' Calculate the binomial mean base on above two.
 #' simulate the cancellations.
 #' for those that cancel, sample new insured from those with the minimal rate for their class.
+#' Set Premium for the round = to the premium from the paired insurer.
