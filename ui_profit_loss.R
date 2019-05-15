d3cdd04a6c1f727b12c9e258733763e0288f1df4
PirateGrunt <captain@pirategrunt.com>
2019-05-15 10:37:32 -0400

    Placeholder for output

--- ui_profit_loss.R
+++ ui_profit_loss.R
@@ -9,8 +9,12 @@ mnu_profit_loss <- menuItem("Profit/Loss", tabName = "tab_profit_loss")
 tab_profit_loss <- tabItem(
   
     "tab_profit_loss"
-  , h3("Tabular results go here")
-  , plotOutput('plt_profit_loss')
+  , fluidRow(
+    dataTableOutput("tbl_profit_loss")
+  )
+  , fluidRow(
+    plotOutput('plt_profit_loss')
+  )
   
 )
 
@@ -19,6 +23,10 @@ tab_profit_loss <- tabItem(
 expr_profit_loss <- quote({
   
   data(mtcars)
+  
+  output$tbl_profit_loss <- renderDataTable({
+   renderDataTable(mtcars) 
+  })
 
   output$plt_profit_loss <- renderPlot({
     mtcars %>% 
