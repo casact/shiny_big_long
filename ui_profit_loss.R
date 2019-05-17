23cb854becbf6b1cdbb6b4fd87ddf0e74af7e763
PirateGrunt <captain@pirategrunt.com>
2019-05-17 16:45:24 -0400

    Actual exhibits with actual data

--- ui_profit_loss.R
+++ ui_profit_loss.R
@@ -2,6 +2,8 @@ library(shiny)
 library(shinydashboard)
 library(tidyverse)
 
+tbl_policyholder_experience <- reactiveVal(tibble())
+
 #=======================================
 # UI ITEMS
 mnu_profit_loss <- menuItem("Profit/Loss", tabName = "tab_profit_loss")
@@ -9,11 +11,18 @@ mnu_profit_loss <- menuItem("Profit/Loss", tabName = "tab_profit_loss")
 tab_profit_loss <- tabItem(
   
     "tab_profit_loss"
-  , fluidRow(
-    dataTableOutput("tbl_profit_loss")
-  )
-  , fluidRow(
-    plotOutput('plt_profit_loss')
+  , conditionalPanel(
+      condition = "output.game_state != 'after'"
+      , fluidRow(
+        plotOutput('plt_profit_loss')
+      )
+      , fluidRow(
+        dataTableOutput("tbl_profit_loss")
+      )
+    )
+  , conditionalPanel(
+    condition = "output.game_state != 'during'"
+    , p("The game has not yet started")
   )
   
 )
@@ -22,16 +31,20 @@ tab_profit_loss <- tabItem(
 # SERVER CODE
 expr_profit_loss <- quote({
   
-  data(mtcars)
-  
   output$tbl_profit_loss <- renderDataTable({
-   renderDataTable(mtcars) 
+    tbl_policyholder_experience() %>% 
+      filter(round_num <= current_round()) %>% 
+      group_by(segment_name, round_num) %>% 
+      summarise(income = sum(income, na.rm = TRUE))
   })
 
   output$plt_profit_loss <- renderPlot({
-    mtcars %>% 
-      ggplot(aes(disp, mpg)) + 
-      geom_point(aes(color = cyl > 6))
+    
+    tbl_policyholder_experience() %>% 
+      filter(round_num <= current_round()) %>% 
+      ggplot(aes(income)) + 
+      geom_histogram() + 
+      facet_wrap(~ round_num, scales = 'free_x')
   })  
 
 })
