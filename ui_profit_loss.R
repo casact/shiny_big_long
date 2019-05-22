fe2e55dde3d721269581cb1b07974a3772a1cad9
JJNewkirk <joshuanewkirk@uwalumni.com>
2019-05-22 12:40:34 +0000

    added indication

--- ui_profit_loss.R
+++ ui_profit_loss.R
@@ -47,14 +47,19 @@ expr_profit_loss <- quote({
                 ,policies=n()
                 ,obs_freq=sum(observed_claims,na.rm=TRUE)/n()
                 ,obs_losses=sum(observed_cost,na.rm=TRUE)
-                ,obs_severity=obs_losses/sum(observed_claims,na.rm=TRUE)) %>%
+                ,total_prem=sum(current_premium)
+                ,obs_severity=obs_losses/sum(observed_claims,na.rm=TRUE))%>%
+    mutate(loss_ratio=obs_losses/total_prem
+           ,indication=loss_ratio-1) %>%
       mutate(income=scales::dollar(income)
              ,avg_premium=scales::comma(avg_premium)
              ,policies=scales::comma(policies)
              ,obs_freq=round(obs_freq,2)
              ,obs_losses=scales::comma(obs_losses)
-             ,obs_severity=scales::comma(obs_severity)) %>%
-      select(segment_name,income,policies,avg_premium,obs_freq,obs_losses)
+             ,obs_severity=scales::comma(obs_severity)
+             ,loss_ratio=scales::percent(loss_ratio)
+             ,indication=scales::percent(indication)) %>%
+      select(segment_name,income,policies,avg_premium,loss_ratio,indication)
       
   })
 
@@ -70,25 +75,34 @@ expr_profit_loss <- quote({
                     ,policies=n()
                     ,obs_freq=sum(observed_claims,na.rm=TRUE)/n()
                     ,obs_losses=sum(observed_cost,na.rm=TRUE)
-                    ,obs_severity=obs_losses/sum(observed_claims,na.rm=TRUE)) %>%
+                    ,total_prem=sum(current_premium)
+                    ,obs_severity=obs_losses/sum(observed_claims,na.rm=TRUE))%>%
+          mutate(loss_ratio=obs_losses/total_prem
+                 ,indication=loss_ratio-1) %>%
           mutate(income=scales::dollar(income)
                  ,avg_premium=scales::comma(avg_premium)
                  ,policies=scales::comma(policies)
                  ,obs_freq=round(obs_freq,2)
                  ,obs_losses=scales::comma(obs_losses)
-                 ,obs_severity=scales::comma(obs_severity))%>%
-        select(segment_name,income,policies,avg_premium,obs_freq,obs_losses)     
-  })
+                 ,obs_severity=scales::comma(obs_severity)
+                 ,loss_ratio=scales::percent(loss_ratio)
+                 ,indication=scales::percent(indication)) %>%
+          select(segment_name,income,policies,avg_premium,loss_ratio,indication)
+    })
   output$plt_profit_loss <- renderPlot({
     
+    data<-
     tbl_policyholder_experience() %>% 
       filter(round_num < current_round(),current_market==player_name()) %>%
       group_by(round_num,segment_name)%>%
       summarise(policies=n()
-             ,obs_loss_cost=sum(observed_cost,na.rm=TRUE)/n()) %>%
+             ,obs_loss_cost=sum(observed_cost,na.rm=TRUE)/n())
+     
+    if(nrow(data)>0){
       ggplot(aes(x=round_num,y=obs_loss_cost)) + 
       geom_boxplot() + 
       facet_wrap(~ segment_name, scales = 'free_x')
+    }
   })  
 
 })
